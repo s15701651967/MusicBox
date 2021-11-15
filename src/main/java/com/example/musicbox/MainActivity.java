f465ef3c25080142956c766782b43b25d4679a30
s15701651967 <61608459+s15701651967@users.noreply.github.com>
2021-11-15 20:45:33 +0800

    Add files via upload

--- src/main/java/com/example/musicbox/MainActivity.java
+++ src/main/java/com/example/musicbox/MainActivity.java
@@ -1,83 +1,83 @@
 package com.example.musicbox;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.radio.R;
 
-import java.util.Random;
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
-    int[] song_id = {R.raw.song1, R.raw.song2, R.raw.song3,R.raw.song4,R.raw.song5};
-    String[] song_name = {"小城故事 — 邓丽君", "Bad day - Danier Powter", "Hozier-Take Me to Church","12 What Lies Unseen - Marcin","15 The Moon over Mount Gorgon - Pitor"};
-    String song_name1="小城君";
-    TextView tw_curTime, tw_totalTime, tw_Song;
-    Button btn_play, btn_pause, btn_stop, btn_next, btn_previous;
-    Button btn_random;
-    static int num = 0;
-    SeekBar seekBar;//时间轴
-
+    int []song_id={R.raw.song1,R.raw.song2,R.raw.song3};
+    String[]song_name={"小城故事 — 邓丽君","Bad day - Danier Powter","Hozier-Take Me to Church"};
+    int songs=0;
+    TextView curTime,totalTime,theSong;
+    Button play,pause,stop,next,previous;
+    static int num=0;
+    SeekBar seekBar;
+    int p=0;
+    Spinner spinner;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = new MediaPlayer();
+        mediaPlayer=new MediaPlayer();
 
-        btn_play = findViewById(R.id.play);
-        btn_pause = findViewById(R.id.pause);
-        btn_stop = findViewById(R.id.stop);
-        btn_next = findViewById(R.id.next);
-        btn_previous = findViewById(R.id.previous);
-        btn_random=findViewById(R.id.random);
+        play=findViewById(R.id.play);
+        pause=findViewById(R.id.pause);
+        stop=findViewById(R.id.stop);
+        next=findViewById(R.id.next);
+        previous=findViewById(R.id.previous);
+        seekBar=findViewById(R.id.mSeekbar);
 
-        seekBar = findViewById(R.id.mSeekbar);
 
-        tw_curTime = findViewById(R.id.curTime);
-        tw_totalTime = findViewById(R.id.totalTime);
-        tw_Song = findViewById(R.id.songName);
-
+        curTime=findViewById(R.id.curTime);
+        totalTime=findViewById(R.id.totalTime);
+        theSong=findViewById(R.id.songName);
         test(num);
-
         initview();
-
         initSong();
 
-        btn_random.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.start();
-                Toast.makeText(MainActivity.this, "随机播放", Toast.LENGTH_SHORT).show();
-                int r=(int)(Math.random()*4);
-                test(r);
-
-            }
-        });
-
-        btn_play.setOnClickListener(new View.OnClickListener() {
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
             }
         });
 
-        btn_pause.setOnClickListener(new View.OnClickListener() {
+        pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.pause();
             }
         });
 
-        btn_stop.setOnClickListener(new View.OnClickListener() {
+        stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.stop();
@@ -85,34 +85,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_next.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {//计算下一首音乐的当前进度及总进度并在布局中更新
-                if (num==4) num = 0;
+            public void onClick(View view) {
+                if(num==2)num=0;
                 else num++;
                 test(num);
-                int total = mediaPlayer.getDuration() / 1000;
-                int curl = mediaPlayer.getCurrentPosition() / 1000;
-                tw_curTime.setText(calculateTime(curl));
-                tw_totalTime.setText(calculateTime(total));
+                int total=mediaPlayer.getDuration()/1000;
+                int curl=mediaPlayer.getCurrentPosition()/1000;
+                curTime.setText(calculateTime(curl));
+                totalTime.setText(calculateTime(total));
                 mediaPlayer.start();
             }
         });
-        btn_previous.setOnClickListener(new View.OnClickListener() {
+        previous.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {//计算上一首音乐的当前进度及总进度并在布局中更新
-                if (num == 0) num = 4;
+            public void onClick(View view) {
+                if(num==0)num=2;
                 else num--;
                 test(num);
-                int total = mediaPlayer.getDuration() / 1000;
-                int curl = mediaPlayer.getCurrentPosition() / 1000;
-                tw_curTime.setText(calculateTime(curl));
-                tw_totalTime.setText(calculateTime(total));
+                int total=mediaPlayer.getDuration()/1000;
+                int curl=mediaPlayer.getCurrentPosition()/1000;
+                curTime.setText(calculateTime(curl));
+                totalTime.setText(calculateTime(total));
                 mediaPlayer.start();
             }
 
         });
-
         mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
@@ -120,12 +119,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        new Thread(new Runnable() {//用于实时更新歌曲播放进度
+        new Thread(new Runnable() {
             @Override
             public void run() {
+                //int total = mediaPlayer.getDuration() / 1000;
+                int curl = mediaPlayer.getCurrentPosition();
                 while(true){
-                    int curl = mediaPlayer.getCurrentPosition() / 10;
-                    tw_curTime.setText(calculateTime(curl/100));
+                    //otal = mediaPlayer.getDuration() / 1000;
+                    curl = mediaPlayer.getCurrentPosition() / 10;
+                    curTime.setText(calculateTime(curl/100));
                     try {
                         Thread.sleep(1000);
                     } catch (Exception e) {
@@ -135,89 +137,93 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
 
+
+
+
     }
 
-    public void initSong() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, song_name);
+    public void initSong(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,song_name);
         ListView lv_1 = findViewById(R.id.listview);
         lv_1.setAdapter(adapter);
-
     }
-
-    public void test(int i) {
-        tw_Song = findViewById(R.id.songName);
-        tw_Song.setText(song_name[i]);
-        if (mediaPlayer != null) {
+    public void test(int i){
+        theSong = findViewById(R.id.songName);
+        theSong.setText(song_name[i]);
+        if(mediaPlayer != null){
             mediaPlayer.stop();
         }
         mediaPlayer = MediaPlayer.create(this, song_id[i]);
-        MediaPlayer  mediaPlayer1=MediaPlayer.create(this,song_id[i+1]);
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                Toast.makeText(MainActivity.this, "单曲循环", Toast.LENGTH_SHORT).show();
-                mediaPlayer1.start();
+                option();
             }
         });
     }
 
+    public void option(){
+        Toast.makeText(MainActivity.this, "finish", Toast.LENGTH_SHORT).show();
 
+    }
 
-    public void initview() {
+    public void initview(){
         int total = mediaPlayer.getDuration() / 1000;
         int curl = mediaPlayer.getCurrentPosition() / 1000;
-        tw_curTime.setText(calculateTime(curl));
-        tw_totalTime.setText(calculateTime(total));
+        curTime.setText(calculateTime(curl));
+        totalTime.setText(calculateTime(total));
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int total = mediaPlayer.getDuration() / 1000;//获取音乐总时长
                 int curl = mediaPlayer.getCurrentPosition() / 1000;//获取当前播放的位置
-                tw_curTime.setText(calculateTime(curl));//开始时间
-                tw_totalTime.setText(calculateTime(total));//总时长
+                curTime.setText(calculateTime(curl));//开始时间
+                totalTime.setText(calculateTime(total));//总时长
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mediaPlayer.seekTo(mediaPlayer.getDuration() * seekBar.getProgress() / 100);//在当前位置播放
-                tw_curTime.setText(calculateTime(mediaPlayer.getCurrentPosition() / 1000));
+                mediaPlayer.seekTo(mediaPlayer.getDuration()*seekBar.getProgress()/100);//在当前位置播放
+                curTime.setText(calculateTime(mediaPlayer.getCurrentPosition() / 1000));
             }
         });
     }
 
-    public String calculateTime(int time) {
+    public String calculateTime(int time){
         int minute;
         int second;
-        if (time > 60) {
+        if(time > 60){
             minute = time / 60;
             second = time % 60;
             //判断秒
-            if (second >= 0 && second < 10) {
-                return "0" + minute + ":" + "0" + second;
-            } else {
-                return "0" + minute + ":" + second;
+            if(second >= 0 && second < 10){
+                return "0"+minute+":"+"0"+second;
+            }else {
+                return "0"+minute+":"+second;
             }
-        } else if (time < 60) {
+        }else if(time < 60){
             second = time;
-            if (second >= 0 && second < 10) {
-                return "00:" + "0" + second;
-            } else {
-                return "00:" + second;
+            if(second >= 0 && second < 10){
+                return "00:"+"0"+second;
+            }else {
+                return "00:"+ second;
             }
-        } else {
+        }else{
             return "01:00";
         }
     }
 
+
+
+
     @Override
-    protected void onDestroy() {
+    protected void onDestroy(){
         super.onDestroy();
-        if (mediaPlayer != null) {
+        if (mediaPlayer!=null)
+        {
             mediaPlayer.stop();
             mediaPlayer.release();
         }
